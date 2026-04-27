573d260ceb9c4421675aea2420764b5c6fbebf14
Iqbal Revianda <iqbalrevianda25@gmail.com>
2026-04-27 16:29:34 +0700

    Fix: Add robust path detection to fix.php

--- fix.php
+++ fix.php
@@ -4,12 +4,22 @@ use Illuminate\Support\Facades\Artisan;
 
 define('LARAVEL_START', microtime(true));
 
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+// Try to find autoloader in current or parent directory
+if (file_exists(__DIR__.'/vendor/autoload.php')) {
+    $basePath = __DIR__;
+} elseif (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    $basePath = __DIR__.'/..';
+} else {
+    die("Could not find vendor/autoload.php. Please make sure fix.php is in the project root or public_html folder.");
+}
+
+require $basePath.'/vendor/autoload.php';
+$app = require_once $basePath.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
 echo "<pre>";
+echo "Using PHP Version: " . phpversion() . "\n";
 echo "Optimizing framework...\n";
 Artisan::call('optimize');
 echo Artisan::output();
