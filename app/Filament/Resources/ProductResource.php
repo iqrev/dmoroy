52a1bd66f655e2bfe9a7a1332f1ef46c1c9368e4
Iqbal Revianda <iqbalrevianda25@gmail.com>
2026-05-26 14:17:35 +0700

    style(admin): move product gallery images upload box to the top of the form

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -42,6 +42,17 @@ class ProductResource extends Resource
     {
         return $schema
             ->components([
+                Schemas\Section::make('Foto Produk')
+                    ->schema([
+                        \Awcodes\Curator\Components\Forms\CuratorPicker::make('galleryImages')
+                            ->relationship('mediaImages', 'id')
+                            ->orderColumn('order')
+                            ->label('Pilih atau Unggah Foto Produk')
+                            ->directory('products')
+                            ->multiple()
+                            ->columnSpanFull(),
+                    ]),
+
                 Schemas\Section::make('Informasi Utama')
                     ->schema([
                         Forms\Components\TextInput::make('name')
@@ -111,13 +122,6 @@ class ProductResource extends Resource
                                     ->required()
                                     ->unique('tags', 'slug'),
                             ]),
-                        \Awcodes\Curator\Components\Forms\CuratorPicker::make('galleryImages')
-                            ->relationship('mediaImages', 'id')
-                            ->orderColumn('order')
-                            ->label('Galeri Foto Produk')
-                            ->directory('products')
-                            ->multiple()
-                            ->columnSpanFull(),
                         Forms\Components\Toggle::make('is_featured')
                             ->label('Tandai sebagai Produk Unggulan')
                             ->required(),
